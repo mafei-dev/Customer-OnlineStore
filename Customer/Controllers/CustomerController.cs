fe73740055db22895296849be84ec94588f84a61
bw-china <bw-china>
2021-04-29 02:39:56 +0530

    Initial commit

---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Customer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Customer.Controllers
+{
+
+    [RoutePrefix("api/customer")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomerController : ApiController
+    {
+        OnlineStoreEntities onlineStoreEntities = new OnlineStoreEntities();
+         
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<string> Get()
+        {
+            string id = Guid.NewGuid().ToString();
+            ItemCategory item = new ItemCategory();
+            item.categoryId = id;
+            item.name = "name";
+            onlineStoreEntities.ItemCategories.Add(item);
+            onlineStoreEntities.SaveChanges();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
